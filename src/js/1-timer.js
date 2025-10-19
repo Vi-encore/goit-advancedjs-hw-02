464fedc320dcb1719aa55619ef83267a3b131dfc
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-10-19 14:35:37 +0300

    тест

---
+++ src/js/1-timer.js
@@ -0,0 +1,23 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+};
+
+console.log(refs.startBtn);
+console.log(refs.input);
+console.log('hell');
